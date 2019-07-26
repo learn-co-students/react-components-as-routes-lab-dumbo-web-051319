4ed76bd70171e577ff4a720142e62a6f2feda053
Tess Neau <tessneau@gmail.com>
2019-07-26 14:42:47 -0400

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const generateActors = actors.map((actor, i) =>(
+    <div key={i}>
+    <h3>Name: {actor.name}</h3>
+    <p>Movies:</p>
+    <ul>
+    {actor.movies.map((movie,i) => <li key={i}>{movie}</li>)}
+    </ul>
+    </div>
+    ))
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {generateActors}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,23 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <li><NavLink to="/" exact activeStyle={{
+        color: 'red'
+      }}>Home
+      </NavLink></li>
+      <li><NavLink to="/actors" exact activeStyle={{
+        color: 'red'
+      }}>Actors
+      </NavLink></li>
+      <li><NavLink to="/directors" exact activeStyle={{
+        color: 'red'
+      }}>Directors
+      </NavLink></li>
+      <li><NavLink to="/movies" exact activeStyle={{
+        color: 'red'
+      }}>Movies
+      </NavLink></li>
     </div>
   );
 };
